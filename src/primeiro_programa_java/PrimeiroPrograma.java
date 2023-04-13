a588bfd0d0d32bb666f247b33ff4adffb73f2d02
moreiraplattini <plattinimoreira@hotmail.com>
2023-04-13 17:56:01 -0300

    First Commit via Eclipse IDE.

---
+++ src/primeiro_programa_java/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package primeiro_programa_java;
+
+import primeiro_programa_java.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		/*int a=2;
+		int b=3;
+		
+		System.out.println("Hello World... " + (a+b));*/
+
+	}
+
+}
